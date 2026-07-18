c2286d02f3e92373086cd3c83005a4e83ac88114
sribash sarkar <sribashsarkarblp@gmail.com>
2026-07-19 02:26:21 +0530

    sribash start commit

--- find_opening_moves.php
+++
@@ -1,7 +0,0 @@
-<?php
-$content = file_get_contents('dating-app (3).sql');
-if (preg_match('/CREATE TABLE `opening_moves`(.*?)ENGINE/is', $content, $matches)) {
-    echo $matches[0];
-} else {
-    echo "Not found";
-}
--- find_users.php
+++
@@ -1,7 +0,0 @@
-<?php
-$content = file_get_contents('dating-app (3).sql');
-if (preg_match('/CREATE TABLE `users`(.*?)ENGINE/is', $content, $matches)) {
-    echo $matches[0];
-} else {
-    echo "Not found";
-}
