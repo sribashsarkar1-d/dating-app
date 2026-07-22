d253704925cb561019a79aba894dc2202ed016f0
sribash sarkar <sribashsarkarblp@gmail.com>
2026-07-22 17:59:54 +0530

    Add safe non-secret fallbacks

--- config/smtp.php
+++ config/smtp.php
@@ -11,9 +11,9 @@ return [
     'host' => $_ENV['SMTP_HOST'] ?? $_SERVER['SMTP_HOST'] ?? getenv('SMTP_HOST') ?: 'smtp.gmail.com',
     'port' => $_ENV['SMTP_PORT'] ?? $_SERVER['SMTP_PORT'] ?? getenv('SMTP_PORT') ?: 587,
     'encryption' => 'tls',
-    'username' => $_ENV['SMTP_USERNAME'] ?? $_SERVER['SMTP_USERNAME'] ?? getenv('SMTP_USERNAME') ?: '',
+    'username' => $_ENV['SMTP_USERNAME'] ?? $_SERVER['SMTP_USERNAME'] ?? getenv('SMTP_USERNAME') ?: 'sribashsarkarblp@gmail.com',
     'password' => $_ENV['SMTP_PASSWORD'] ?? $_SERVER['SMTP_PASSWORD'] ?? getenv('SMTP_PASSWORD') ?: '',
     'from_name' => $_ENV['SMTP_FROM_NAME'] ?? $_SERVER['SMTP_FROM_NAME'] ?? getenv('SMTP_FROM_NAME') ?: 'Dating App',
-    'from_email' => $_ENV['SMTP_FROM_EMAIL'] ?? $_SERVER['SMTP_FROM_EMAIL'] ?? getenv('SMTP_FROM_EMAIL') ?: '',
+    'from_email' => $_ENV['SMTP_FROM_EMAIL'] ?? $_SERVER['SMTP_FROM_EMAIL'] ?? getenv('SMTP_FROM_EMAIL') ?: 'sribashsarkarblp@gmail.com',
     'brevo_api_key' => $_ENV['BREVO_API_KEY'] ?? $_SERVER['BREVO_API_KEY'] ?? getenv('BREVO_API_KEY') ?: ''
 ];
