bab50d6eac212d79a778c3c4e15b9ccf84cc2b8e
sribash sarkar <sribashsarkarblp@gmail.com>
2026-07-22 18:35:28 +0530

    sribashnor start commit

--- config/smtp.php
+++ config/smtp.php
@@ -12,7 +12,7 @@ return [
     'port' => $_ENV['SMTP_PORT'] ?? $_SERVER['SMTP_PORT'] ?? getenv('SMTP_PORT') ?: 587,
     'encryption' => 'tls',
     'username' => $_ENV['SMTP_USERNAME'] ?? $_SERVER['SMTP_USERNAME'] ?? getenv('SMTP_USERNAME') ?: 'sribashsarkarblp@gmail.com',
-    'password' => $_ENV['SMTP_PASSWORD'] ?? $_SERVER['SMTP_PASSWORD'] ?? getenv('SMTP_PASSWORD') ?: '',
+    'password' => $_ENV['SMTP_PASSWORD'] ?? $_SERVER['SMTP_PASSWORD'] ?? getenv('SMTP_PASSWORD') ?: 'hjdxusaxmpwcwsgk',
     'from_name' => $_ENV['SMTP_FROM_NAME'] ?? $_SERVER['SMTP_FROM_NAME'] ?? getenv('SMTP_FROM_NAME') ?: 'Dating App',
     'from_email' => $_ENV['SMTP_FROM_EMAIL'] ?? $_SERVER['SMTP_FROM_EMAIL'] ?? getenv('SMTP_FROM_EMAIL') ?: 'sribashsarkarblp@gmail.com',
     'brevo_api_key' => $_ENV['BREVO_API_KEY'] ?? $_SERVER['BREVO_API_KEY'] ?? getenv('BREVO_API_KEY') ?: ''
