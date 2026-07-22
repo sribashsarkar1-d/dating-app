e475ee638f0ade717aa72d09f7e5a91daf44a71c
sribash sarkar <sribashsarkarblp@gmail.com>
2026-07-22 17:53:49 +0530

    Add dotenv support to protect secrets

--- config/smtp.php
+++ config/smtp.php
@@ -1,10 +1,19 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
+
 return [
-    'host' => 'smtp.gmail.com',
-    'port' => 587,
+    'host' => $_ENV['SMTP_HOST'] ?? 'smtp.gmail.com',
+    'port' => $_ENV['SMTP_PORT'] ?? 587,
     'encryption' => 'tls',
-    'username' => 'sribashsarkarblp@gmail.com',
-    'password' => 'hjdxusaxmpwcwsgk',
-    'from_name' => 'Dating App',
-    'from_email' => 'sribashsarkarblp@gmail.com'
+    'username' => $_ENV['SMTP_USERNAME'] ?? '',
+    'password' => $_ENV['SMTP_PASSWORD'] ?? '',
+    'from_name' => $_ENV['SMTP_FROM_NAME'] ?? 'Dating App',
+    'from_email' => $_ENV['SMTP_FROM_EMAIL'] ?? '',
+    'brevo_api_key' => $_ENV['BREVO_API_KEY'] ?? ''
 ];
